6086b3d401bf21ac7a354df6b7218610e801ab1f
LitoralVerde <136463023+LitoralVerde@users.noreply.github.com>
2024-11-26 10:40:27 -0300

    desafio felipão

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-//criando arquivo
+const nomeHeroi = prompt("Digite o nome do herói:");
+const xpHeroi = parseInt(prompt("Digite a quantidade de XP do herói:"));
+
+const nivel = xpHeroi < 1000 ? "Ferro"
+    : xpHeroi <= 2000 ? "Bronze"
+    : xpHeroi <= 5000 ? "Prata"
+    : xpHeroi <= 7000 ? "Ouro"
+    : xpHeroi <= 8000 ? "Platina"
+    : xpHeroi <= 9000 ? "Ascendente"
+    : xpHeroi <= 10000 ? "Imortal"
+    : "Radiante";
+
+alert(`O herói ${nomeHeroi} possui ${xpHeroi} de XP e está no nível ${nivel}.`);
